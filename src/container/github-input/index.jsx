bbb724b90931ffe6ca4e92737faf965948e2e8f6
vinocation <vinocation.x@gmail.com>
2021-12-17 00:08:11 +0800

    add css module

--- src/container/github-input/index.jsx
+++ src/container/github-input/index.jsx
@@ -1,6 +1,7 @@
 import { Component } from "react";
 import { fromEvent, from } from "rxjs";
 import { filter, take } from "rxjs/operators";
+import style from "./index.module.css";
 
 class GithubInput extends Component {
   onChange = (e) => {
@@ -10,12 +11,12 @@ class GithubInput extends Component {
   componentDidMount() {
     const range = from([1, 2, 3, 4, 5]);
     // 创建发出点击事件的 observable
-    const source = fromEvent(document.getElementById("miranda"), "input");
+    const source = fromEvent(document.getElementById("search-btn"), "input");
     // filter
     const filterSource = source.pipe(filter((e) => e.target.value.length > 2));
     // take
     const takeSource = source.pipe(take(5));
-    takeSource.subscribe((x) => console.log("rxjs:", x.target.value));
+    takeSource.subscribe((x) => console.log("take log", x.target.value));
   }
 
   componentWillUnmount() {
@@ -24,11 +25,19 @@ class GithubInput extends Component {
 
   render() {
     return (
-      <input
-        type="text"
-        id="miranda"
-        //  onKeyUp={this.onChange}
-      ></input>
+      <div className={style.mainContainer}>
+        <div className={style.header}>
+          <input
+            className={style.search}
+            id="search-btn"
+            type="text"
+            maxLength="1000"
+            required=""
+            placeholder="search in github"
+            //  onKeyUp={this.onChange}
+          />
+        </div>
+      </div>
     );
   }
 }
